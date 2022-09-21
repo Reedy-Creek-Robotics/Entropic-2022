81fd0913c4f62b929c89683da090ca2d9b398aac
samuelstephenson <samuel.r.stephenson@gmail.com>
2022-09-21 16:35:20 -0400

    Updated to work if there are no servos or motors listed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ServoMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ServoMotorTest.java
@@ -38,8 +38,9 @@ public class ServoMotorTest extends OpMode {
         for (String name : motorNames) {
             motors.add(motor(name));
         }
+
         servos = new ArrayList<>();
-        for(String name : servoNames) {
+        for (String name : servoNames) {
             servos.add(servo(name));
         }
 
@@ -71,31 +72,46 @@ public class ServoMotorTest extends OpMode {
             currentServo--;
             currentServo = currentServo % servos.size();
         }
+        
+        if(motors.size() !=0 ) {
+            //Displays the current motor in telemetry
+            telemetry.addData("Current Motor", motorNames.get(currentMotor));
+            telemetry.addData("Current motor power",gamepad1.left_stick_y);
 
-        //Displays the current motor and servo in telemetry
-        telemetry.addData("Current Motor", motorNames.get(currentMotor));
-        telemetry.addData("Current motor power",gamepad1.left_stick_y);
+            //Sets the power and position of the current motor and servo
+            motors.get(currentMotor).setPower(gamepad1.left_stick_y);
 
-        telemetry.addData("Current Servo",servoNames.get(currentServo));
-        telemetry.addData("Current servo position",gamepad2.left_stick_y);
 
-        //Sets the power and position of the current motor and servo
-        motors.get(currentMotor).setPower(gamepad1.left_stick_y);
-        servos.get(currentServo).setPosition(gamepad2.left_stick_y);
-
-        //Makes motors not in use stop running
-        for(DcMotorEx motor: motors) {
-            if(motor != motors.get(currentMotor)) {
-                motor.setPower(0);
+            //Makes motors not in use stop running
+            for(DcMotorEx motor: motors) {
+                if(motor != motors.get(currentMotor)) {
+                    motor.setPower(0);
+                }
             }
+        }else {
+            telemetry.addData("No Motor","");
         }
 
-        //Makes servos not in use go to its 0 position
-        for(Servo servo : servos) {
-            if(servo != servos.get(currentServo)) {
-                servo.setPosition(0);
+        if(servos.size() != 0) {
+
+            //Displays the current servo in telemetry
+            telemetry.addData("Current Servo",servoNames.get(currentServo));
+            telemetry.addData("Current servo position",gamepad2.left_stick_y);
+
+
+            //Sets the position of the current servo
+            servos.get(currentServo).setPosition(gamepad2.left_stick_y);
+
+            //Makes servos not in use go to its 0 position
+            for(Servo servo : servos) {
+                if(servo != servos.get(currentServo)) {
+                    servo.setPosition(0);
+                }
             }
+        }else {
+            telemetry.addData("No Servo","");
         }
+
 
         telemetry.update();
     }
