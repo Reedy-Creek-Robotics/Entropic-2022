a86ecd3bcbb531542da7e2c476dc22d9320dab3f
samuelstephenson <samuel.r.stephenson@gmail.com>
2022-11-22 18:37:50 -0500

    Added base hardware components.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class Intake extends BaseComponent{
+    private CRServo intakeServo;
+
+    public Intake(OpMode opMode) {
+        super(opMode);
+        intakeServo = hardwareMap.crservo.get("intakeWheel");
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    /**
+     * @param power of the servo
+     */
+    public void intake(double power) {
+        executeCommand(new IntakeCone(power));
+    }
+
+    /**
+     * Stops the intake
+     */
+    public void stopIntake() {
+        intakeServo.setPower(0);
+    }
+
+    private abstract class BaseCommand implements Command {
+        //time in seconds
+        double timeToSpin;
+
+        double power;
+
+        public BaseCommand(double timeToSpin, double power) {
+            this.timeToSpin = timeToSpin;
+            this.power = power;
+        }
+
+        @Override
+        public void stop() {
+            stopIntake();
+        }
+
+        @Override
+        public boolean updateStatus() {
+            return time.seconds() > timeToSpin;
+        }
+    }
+
+    private class IntakeCone extends BaseCommand {
+
+        public IntakeCone(double power) {
+            super(.5,power);
+        }
+
+        @Override
+        public void start() {
+            time.reset();
+            intakeServo.setPower(power);
+        }
+    }
+
+    @Override
+    public void updateStatus() {
+        super.updateStatus();
+    }
+
+    @Override
+    public boolean isBusy() {
+        return super.isBusy();
+    }
+}
