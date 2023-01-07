f7f523ce796e894bb4a56b050c4edeff95807d3a
Sean Stephenson <sean.stephenson@taulia.com>
2023-01-07 14:54:57 -0500

    Strafe correction calibration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calibration/RampingCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calibration/RampingCalibration.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.calibration;
 
 import static org.firstinspires.ftc.teamcode.Controller.AnalogControl.LEFT_STICK_Y;
+import static org.firstinspires.ftc.teamcode.Controller.AnalogControl.LEFT_TRIGGER;
+import static org.firstinspires.ftc.teamcode.Controller.AnalogControl.RIGHT_TRIGGER;
 import static org.firstinspires.ftc.teamcode.Controller.Button.DPAD_DOWN;
 import static org.firstinspires.ftc.teamcode.Controller.Button.DPAD_LEFT;
 import static org.firstinspires.ftc.teamcode.Controller.Button.DPAD_RIGHT;
@@ -13,16 +15,20 @@ import static org.firstinspires.ftc.teamcode.components.DriveTrain.Direction.Y;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.BaseTeleOp;
+import org.firstinspires.ftc.teamcode.RobotDescriptor.EmpiricalStrafeCorrection;
+
+import java.util.Collections;
 
 @TeleOp(group = "Calibration")
 public class RampingCalibration extends BaseTeleOp {
 
+    private double strafeCorrection = 1.0;
     private double limiter = 0.3;
 
     @Override
     public void loop() {
 
-        //Moving the robot
+        // Moving the robot
         if (controller.isPressed(DPAD_UP)) {
             robot.getDriveTrain().moveAlignedToTileCenter(1, Y, limiter);
         } else if (controller.isPressed(DPAD_LEFT)) {
@@ -35,12 +41,24 @@ public class RampingCalibration extends BaseTeleOp {
             limiter += 0.05;
         } else if (controller.isPressed(LEFT_BUMPER) && limiter > 0) {
             limiter -= 0.05;
+        } else if (controller.isPressed(RIGHT_TRIGGER)) {
+            strafeCorrection += 0.01 * controller.analogValue(RIGHT_TRIGGER);
+        } else if (controller.isPressed(LEFT_TRIGGER)) {
+            strafeCorrection -= 0.01 * controller.analogValue(LEFT_TRIGGER);
         }
+
+        // Overwrite the empirical strafe corrections with a single value to force it to be used.
+        descriptor.empiricalStrafeCorrections = Collections.singletonList(
+                new EmpiricalStrafeCorrection(limiter, strafeCorrection)
+        );
 
         //changing ramping values
         if (controller.isPressed(LEFT_STICK_Y)) {
             robot.getRobotContext().robotDescriptor.rampingUpMinMotorPower += controller.leftStickY() * 0.01;
         }
+
+        telemetry.addData("Limiter", limiter);
+        telemetry.addData("Strafe Correction", strafeCorrection);
 
         robot.updateStatus();
     }
