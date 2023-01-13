f6fb09972cba41da7f9e84b38ffca6abbac09f0d
Sean Stephenson <sean.stephenson@taulia.com>
2023-01-13 13:09:38 -0500

    Adding Hough statistics so we can learn how well it is working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTrain.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.components;
 import static org.firstinspires.ftc.teamcode.components.DriveTrain.Direction.FIELD_X;
 import static org.firstinspires.ftc.teamcode.components.DriveTrain.Direction.FIELD_Y;
 import static org.firstinspires.ftc.teamcode.util.DistanceUtil.inchesToTiles;
+import static org.firstinspires.ftc.teamcode.util.DistanceUtil.tilesToInches;
 import static org.firstinspires.ftc.teamcode.util.RobotFieldConversionUtil.FieldSpaceCoordinates;
 import static org.firstinspires.ftc.teamcode.util.RobotFieldConversionUtil.RobotSpaceCoordinates;
 import static org.firstinspires.ftc.teamcode.util.RobotFieldConversionUtil.convertToFieldSpace;
@@ -93,6 +94,11 @@ public class DriveTrain extends BaseComponent implements RobotPositionProvider {
      * The previously seen tile edge observation.  We don't want to apply the same observation twice.
      */
     private TileEdgeSolver.TileEdgeObservation previousObservation;
+
+    /**
+     * Statistics about Hough tile edge correction.
+     */
+    private HoughStatistics houghStatistics = new HoughStatistics();
 
 
     public DriveTrain(RobotContext context, WebCam webCamSide) {
@@ -199,6 +205,8 @@ public class DriveTrain extends BaseComponent implements RobotPositionProvider {
         //telemetry.addData("Current Command", getCurrentCommand());
         //telemetry.addData("Next Commands", getNextCommands());
 
+        telemetry.addData("Hough Stats", houghStatistics);
+
         // Now allow any commands to run with the updated data
         super.updateStatus();
     }
@@ -258,9 +266,13 @@ public class DriveTrain extends BaseComponent implements RobotPositionProvider {
 
             // Then using the observation overwrite the expected with the actual values.
             if (observation.distanceRight != null) {
+                houghStatistics.rightEdgeCorrections++;
+                houghStatistics.rightEdgeCorrectionDistance += Math.abs(observation.distanceRight - robotSpaceCoordinates.distanceRight);
                 robotSpaceCoordinates.distanceRight = observation.distanceRight;
             }
             if (observation.distanceFront != null) {
+                houghStatistics.frontEdgeCorrections++;
+                houghStatistics.frontEdgeCorrectionDistance += Math.abs(observation.distanceFront - robotSpaceCoordinates.distanceFront);
                 robotSpaceCoordinates.distanceFront = observation.distanceFront;
             }
             if (observation.headingOffset != null) {
@@ -292,6 +304,16 @@ public class DriveTrain extends BaseComponent implements RobotPositionProvider {
                 // Also update the previous position by the same amount, so the velocity doesn't jump.
                 if (previousPosition != null) {
                     previousPosition = previousPosition.add(correction);
+                }
+
+                houghStatistics.totalCorrections++;
+                houghStatistics.totalCorrectionDistance += correctionDistance;
+                houghStatistics.totalObservationAge += observation.observationTime.seconds();
+
+                if (velocity != null && velocity.magnitude() > 0.01) {
+                    houghStatistics.movingCorrections++;
+                } else {
+                    houghStatistics.stationaryCorrections++;
                 }
             }
         }
@@ -1149,6 +1171,35 @@ public class DriveTrain extends BaseComponent implements RobotPositionProvider {
                     backLeft, backRight, frontLeft, frontRight
             );
         }
+    }
+
+    private class HoughStatistics {
+
+        public double totalCorrectionDistance;
+        public int totalCorrections;
+        public double totalObservationAge;
+        public double frontEdgeCorrectionDistance;
+        public int frontEdgeCorrections;
+        public double rightEdgeCorrectionDistance;
+        public int rightEdgeCorrections;
+        public int stationaryCorrections;
+        public int movingCorrections;
+
+        @Override
+        public String toString() {
+            double averageObservationAge = totalCorrections != 0 ?
+                    totalObservationAge / totalCorrections :
+                    0.0;
+
+            return String.format(
+                    "Total [%.1f in, %d], Front [%.1f in, %d], Right [%.1f in, %d], Stationary [%d], Moving [%d], Avg Age [%.3f s]",
+                    tilesToInches(totalCorrectionDistance), totalCorrections,
+                    tilesToInches(frontEdgeCorrectionDistance), frontEdgeCorrections,
+                    tilesToInches(rightEdgeCorrectionDistance), rightEdgeCorrections,
+                    stationaryCorrections, movingCorrections, averageObservationAge
+            );
+        }
+
     }
 
 }
