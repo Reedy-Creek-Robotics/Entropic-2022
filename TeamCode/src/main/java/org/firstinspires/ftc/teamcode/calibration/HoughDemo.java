80eafb9bc5487aa8290dd29b4c61ce784c824d0b
SmartEntity <68695760+SmartEntity@users.noreply.github.com>
2023-04-29 15:20:42 -0400

    Created a Hough to display at outreach events

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calibration/HoughDemo.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.calibration;
+
+import static org.firstinspires.ftc.teamcode.Controller.*;
+
+import org.firstinspires.ftc.teamcode.BaseDrivingTeleOp;
+import org.firstinspires.ftc.teamcode.components.Robot;
+import org.firstinspires.ftc.teamcode.geometry.Heading;
+import org.firstinspires.ftc.teamcode.geometry.Position;
+import org.firstinspires.ftc.teamcode.util.DistanceUtil;
+
+public class HoughDemo extends BaseDrivingTeleOp {
+    @Override
+    protected Robot.Camera getStreamingCamera() {
+        return Robot.Camera.SIDE;
+    }
+
+    @Override
+    public void loop() {
+
+        if (controller.isPressed(Button.A)) {
+            double x = DistanceUtil.inchesToTiles(robot.getRobotContext().robotDescriptor.robotDimensionsInInches.width / 2);
+            robot.getDriveTrain().moveToTargetPosition(new Position(x, 0.5), new Heading(90), 0.4);
+        }
+
+        applyBasicDriving();
+    }
+}
