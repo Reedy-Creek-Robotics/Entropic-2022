22bae59b781405025b43e373e1ecda1e2c15ac30
sawyer <sawyer.mellon@gmail.com>
2023-10-24 20:44:28 -0400

    Made a color detector to help detect the team prop Started a basic version of the hardware tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/TeamPropDetector.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.components;
+
+import org.firstinspires.ftc.teamcode.components.ColorDetector.ColorDetection;
+import org.firstinspires.ftc.teamcode.geometry.Position;
+import org.firstinspires.ftc.teamcode.geometry.Rectangle;
+
+import java.util.List;
+
+public class TeamPropDetector extends BaseComponent {
+
+    public enum TeamPropPosition {
+        LEFT(new Rectangle(new Position(100, 100), 75, 75)),
+        MIDDLE(new Rectangle(new Position(300, 100), 75, 75)),
+        RIGHT(new Rectangle(new Position(500, 100), 75, 75));
+
+        public Rectangle rectangle;
+
+        TeamPropPosition(Rectangle rectangle) {
+            this.rectangle = rectangle;
+        }
+    }
+
+    /**
+     * The color detector to use to find the team prop.
+     */
+    private ColorDetector colorDetector;
+
+    public TeamPropPosition getDetectedPosition() {
+        // Get the latest detections from the color detector
+        List<ColorDetection> detections = this.colorDetector.getDetections();
+
+
+
+        return null;
+    }
+
+    public TeamPropDetector(RobotContext context, WebCam webCam) {
+        super(context);
+
+        this.colorDetector = new ColorDetector(context, webCam);
+        addSubComponents(colorDetector);
+    }
+
+}
