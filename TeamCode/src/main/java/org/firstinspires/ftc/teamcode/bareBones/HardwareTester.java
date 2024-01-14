3a5fdd7ecd820735dabc72a100200b5f9719f177
sawyer <sawyer.mellon@gmail.com>
2024-01-14 15:50:00 -0500

    finished hardware tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bareBones/HardwareTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bareBones/HardwareTester.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.bareBones;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.game.Controller;
@@ -13,84 +14,72 @@ import java.util.List;
 public class HardwareTester extends OpMode {
 
     private Controller controller;
-    private List<DcMotorEx> motors = new ArrayList<>();
-    private int selectedMotorIndex = 0;
+    private int selectedDevice = 0;
 
-    private DcMotorEx Motor1;
-    private DcMotorEx Motor2;
-    private DcMotorEx Motor3;
-    private DcMotorEx Motor4;
+    private List<HardwareDevice> devices;
 
-    private boolean MotorControl;
-
-    private List<Servo> servos = new ArrayList<>();
-    private int selectedServoIndex = 0;
-
-    private Servo Servo1;
-    private Servo Servo2;
-    private Servo Servo3;
-    private Servo Servo4;
-
-    private boolean ServoControl;
+    double targetPosition;
 
     @Override
     public void init() {
+        devices = new ArrayList<>();
+        for (HardwareDevice device : hardwareMap) {
+            devices.add(device);
+        }
+
         controller = new Controller(gamepad1);
-
-        for (DcMotor dcMotor : hardwareMap.dcMotor) {
-            motors.add((DcMotorEx) dcMotor);
-
-        }
-        for (Servo servo : hardwareMap.servo) {
-            servos.add((Servo) servo);
-
-        }
     }
 
     @Override
     public void loop() {
-        // changes from testing motors to servos with dpad up
-        if (controller.isPressed(Controller.Button.DPAD_UP)) {
-            MotorControl = true;
-            ServoControl = false;
-        // changes from testing servos to motors with dpad down
-        } else if (controller.isPressed(Controller.Button.DPAD_DOWN)) {
-            MotorControl = false;
-            ServoControl = true;
-        }
-        if (MotorControl) {
-            // This changes the selected motor
-            if (controller.isPressed(Controller.Button.DPAD_RIGHT)) {
-                selectedMotorIndex++;
-            } else if (controller.isPressed(Controller.Button.DPAD_LEFT)) {
-                selectedMotorIndex--;
-            } else if (controller.isPressed(Controller.Button.DPAD_RIGHT) && selectedMotorIndex == 3) {
-                selectedMotorIndex = 0;
-            } else if (controller.isPressed(Controller.Button.DPAD_LEFT) && selectedMotorIndex == 0) {
-                selectedMotorIndex = 3;
-            }
-        } else if (ServoControl){
-            // This changes the selected servo
-            if (controller.isPressed(Controller.Button.DPAD_RIGHT)) {
-                selectedServoIndex++;
-            } else if (controller.isPressed(Controller.Button.DPAD_LEFT)) {
-                selectedServoIndex--;
-            } else if (controller.isPressed(Controller.Button.DPAD_RIGHT) && selectedServoIndex == 3) {
-                selectedServoIndex = 0;
-            } else if (controller.isPressed(Controller.Button.DPAD_LEFT) && selectedMotorIndex == 0) {
-                selectedServoIndex = 3;
-            }
+        // Output the selected device to telemetry
+        HardwareDevice device = devices.get(selectedDevice);
+
+        telemetry.addData("Device Count", selectedDevice + " of " + devices.size());
+        telemetry.addData("Device", device.getDeviceName());
+        telemetry.addData("Type", device.getClass().getSimpleName());
+
+        if (controller.isPressed(Controller.Button.RIGHT_BUMPER)) {
+            selectedDevice++;
+            if (selectedDevice >= devices.size()) selectedDevice = 0;
+        } else if (controller.isPressed(Controller.Button.LEFT_BUMPER)) {
+            selectedDevice--;
+            if (selectedDevice < 0) selectedDevice = devices.size() - 1;
         }
 
-        // This gets the motors hardware map name
-        DcMotorEx selectedMotor = motors.get(selectedMotorIndex);
-        String motorName = hardwareMap.getNamesOf(selectedMotor).iterator().next();
-        telemetry.addData("Motor", motorName);
+        if (device instanceof DcMotorEx) {
+            DcMotorEx motor = (DcMotorEx) device;
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // This gets the servos hardware map name
-        Servo selectedServo = servos.get(selectedServoIndex);
-        String servoName = hardwareMap.getNamesOf(selectedServo).iterator().next();
-        telemetry.addData("Servo", servoName);
+            telemetry.addData("Position", motor.getCurrentPosition());
+
+            if (controller.isPressed(Controller.AnalogControl.LEFT_STICK_Y)) {
+                double power = controller.leftStickY();
+                motor.setPower(power);
+            }
+
+            if (controller.isPressed(Controller.Button.CIRCLE)) {
+                motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            }
+
+        } else if (device instanceof Servo) {
+            Servo servo = (Servo) device;
+
+            telemetry.addData("Position", servo.getPosition());
+
+            if (controller.isPressed(Controller.AnalogControl.RIGHT_STICK_Y)) {
+                targetPosition += controller.analogValue(Controller.AnalogControl.RIGHT_STICK_Y) * 0.01;
+                servo.setPosition(targetPosition);
+            }
+
+            if (controller.isPressed(Controller.Button.CIRCLE)) {
+                targetPosition = 0;
+                servo.setPosition(targetPosition);
+            }
+
+        }
+
+        telemetry.update();
     }
 
 }
